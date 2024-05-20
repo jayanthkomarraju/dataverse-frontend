0acfa43008fe91f8b6cd9f5213c6a5254e28509e
jayanthkomarraju <59875043+jayanthkomarraju@users.noreply.github.com>
2024-05-20 14:26:01 -0400

    Update App.tsx
    - Added the Language Context and Global Styles to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { SessionProvider } from './sections/session/SessionProvider'
 import { UserJSDataverseRepository } from './users/infrastructure/repositories/UserJSDataverseRepository'
 import { DataverseApiAuthMechanism } from '@iqss/dataverse-client-javascript/dist/core/infra/repositories/ApiConfig'
 import { BASE_URL } from './config'
+import { LanguageProvider } from './shared/LanguageContext';
+import './assets/styles/global.scss';
 
 if (BASE_URL === '') {
   throw Error('VITE_DATAVERSE_BACKEND_URL environment variable should be specified.')
@@ -14,9 +16,11 @@ if (BASE_URL === '') {
 const userRepository = new UserJSDataverseRepository()
 function App() {
   return (
-    <SessionProvider repository={userRepository}>
-      <Router />
-    </SessionProvider>
+    <LanguageProvider>
+      <SessionProvider repository={userRepository}>
+        <Router />
+      </SessionProvider>
+    </LanguageProvider>
   )
 }
 
