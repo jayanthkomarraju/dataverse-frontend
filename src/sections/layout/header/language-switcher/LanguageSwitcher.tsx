b2ca59bb28ea1c334cd88cd3c998e0dc2021c9d1
jayanthkomarraju <59875043+jayanthkomarraju@users.noreply.github.com>
2024-05-22 17:37:00 -0400

    Update LanguageSwitcher.tsx
    Added AR to the language dropdown

--- src/sections/layout/header/language-switcher/LanguageSwitcher.tsx
+++ src/sections/layout/header/language-switcher/LanguageSwitcher.tsx
@@ -2,27 +2,39 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Navbar } from '@iqss/dataverse-design-system';
 
-
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
-
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
 
+  const getLanguageName = (lng: string) => {
+    switch (lng) {
+      case 'en':
+        return 'English';
+      case 'fr':
+        return 'Français';
+      case 'ar':
+        return 'العربية';
+      default:
+        return 'English';
+    }
+  };
 
   return (
-    <Navbar.Dropdown title={i18n.language === 'en' ? 'English' : 'Français'} id="dropdown-language">
+    <Navbar.Dropdown title={getLanguageName(i18n.language)} id="dropdown-language">
       <Navbar.Dropdown.Item href="#" onClick={() => changeLanguage('en')}>
         English
       </Navbar.Dropdown.Item>
       <Navbar.Dropdown.Item href="#" onClick={() => changeLanguage('fr')}>
         Français
       </Navbar.Dropdown.Item>
+      <Navbar.Dropdown.Item href="#" onClick={() => changeLanguage('ar')}>
+        العربية
+      </Navbar.Dropdown.Item>
     </Navbar.Dropdown>
   );
 };
-
 
 export default LanguageSwitcher;
